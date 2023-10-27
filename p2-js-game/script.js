94914d7de3e0d2af1cab2370dde6fd02f55d8887
JM <jmdsegismundo@gmail.com>
2023-10-27 21:39:17 +0800

    feat: javascript project

---
+++ p2-js-game/script.js
@@ -0,0 +1,366 @@
+const player1CardsContainer = document.querySelector('.player1-cards');
+const player2CardsContainer = document.querySelector('.player2-cards');
+const endTurnButton = document.querySelector('.end-turn-button');
+const player1Life = document.querySelector('.player1-LP');
+const player2Life = document.querySelector('.player2-LP');
+const player1EnergyDisplay = document.querySelector('.player1-energy');
+const player2EnergyDisplay = document.querySelector('.player2-energy');
+const restartGameButton = document.querySelector('.restart-game-button');
+const homeButton = document.querySelector('.home-button');
+
+let player1LifePoints = 100;
+let player2LifePoints = 100;
+let player1Energy = 5;
+let player2Energy = 5;
+
+const totalCards = 3;
+
+const cardContent = [
+    { name: 'Kapre', atk: 18, energy: 5, image: 'resources/kapre.png' },
+    { name: 'Engkanto', atk: 15, energy: 4, image: 'resources/engkanto.png' },
+    { name: 'Tikbalang', atk: 13, energy: 3, image: 'resources/tikbalang.png' },
+    { name: 'Aswang', atk: 10, energy: 3, image: 'resources/aswang.png' },
+    { name: 'Sigbin', atk: 7, energy: 2, image: 'resources/sigbin.png' },
+    { name: 'Dwende', atk: 5, energy: 2, image: 'resources/dwende.png' },
+    { name: 'Tiyanak', atk: 3, energy: 1, image: 'resources/tiyanak.png' },
+    { name: 'Trap Card 1', atk: 10, energy: 2, image: 'resources/kulam.png' },
+    { name: 'Trap Card 2', atk: -5, energy: 2, image: 'resources/sumpa.png' },
+    { name: 'Trap Card 3', atk: 0, energy: 2, image: 'resources/agimat.png' },
+
+    ////////add more 2 more kulam and sumpa cards to deck////////
+];
+
+const back = { image: 'resources/back.png' };
+const trap = { image: 'resources/trap.png' };
+
+
+const startGameButton = document.querySelector('.start-button');
+
+function startGame() {
+
+    const container = document.querySelector('.game-container');
+    const homescreen = document.querySelector('.home-screen');
+    const playerNameInput = document.querySelector('.input-player-name');
+    const player1Name = document.querySelector('.player1-name');
+
+    const playerName = playerNameInput.value;
+
+
+    if (playerName) {
+        player1Name.textContent = playerName;
+    };
+
+    setTimeout(() => {
+        container.style.display = 'block';
+        homescreen.style.display = 'none';
+    }, 500);
+
+    player2CardsContainer.innerHTML = '';
+    for (let i = 0; i < totalCards; i++) {
+        const card = document.createElement('img');
+        card.className = 'player2-card';
+        card.src = 'resources/back.png';
+        player2CardsContainer.appendChild(card);
+    }
+
+    showTurn();
+    setTimeout(player1Turn, 500);
+};
+
+startGameButton.addEventListener('click', startGame);
+
+
+function showTurn() {
+
+    const turn1 = document.querySelector('.player1-turn');
+    const turn2 = document.querySelector('.enemy-turn');
+
+    if (currentPlayer === 1) {
+        turn1.style.display = 'block';
+        turn2.style.display = 'none';
+    } else {
+        turn2.style.display = 'block';
+        turn1.style.display = 'none';
+    }
+};
+
+
+const howToPlay = document.querySelector('.how-to-play-button');
+const popupWindow = document.querySelector('.popup-window');
+const closeButton = document.querySelector('.close-button');
+
+howToPlay.addEventListener('click', function () {
+    popupWindow.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function () {
+    popupWindow.style.display = 'none';
+});
+
+
+function assignRandomCardsToPlayer(cardsContainer, player) {
+    cardsContainer.innerHTML = '';
+    for (let i = 0; i < totalCards; i++) {
+        const card = createRandomCard(player);
+        cardsContainer.appendChild(card);
+    }
+}
+
+
+let currentPlayer = 1;
+
+function createRandomCard(player) {
+
+    const card = document.createElement('img');
+    card.className = `player${player}-card`;
+
+    const randomIndex = Math.floor(Math.random() * cardContent.length);
+    const cardData = cardContent[randomIndex];
+
+    card.src = cardData.image;
+    card.textContent = `${cardData.name} (ATK: ${cardData.atk}, Energy: ${cardData.energy})`;
+
+    card.used = false;
+
+
+    if (cardData.name.startsWith('Trap Card')) {
+
+        card.src = trap.image;
+        card.textContent = `${cardData.name} (ATK: ${cardData.atk}, Energy: ${cardData.energy})`;
+
+        if (currentPlayer === 2) {
+            card.src = cardData.image;
+        } else {
+
+            card.addEventListener('click', function () {
+                if (!card.used) {
+                    card.src = cardData.image;
+                    card.textContent = `${cardData.name} (ATK: ${cardData.atk}, Energy: ${cardData.energy})`;
+
+                    if (player === 1 && player1Energy >= cardData.energy) {
+                        useCard(cardData.atk, player);
+                        if (player === 1) {
+                            player1Energy -= cardData.energy;
+                            updateEnergyDisplay(player1EnergyDisplay, player1Energy);
+                        }
+
+                        card.classList.add('contrast');
+                        card.used = true;
+                    }
+                }
+            });
+        }
+
+    } else {
+        card.src = cardData.image;
+        card.textContent = `${cardData.name} (ATK: ${cardData.atk}, Energy: ${cardData.energy})`;
+
+        card.addEventListener('click', function () {
+            if (currentPlayer === player) {
+                if ((player === 1 && player1Energy >= cardData.energy) || (player === 2 && player2Energy >= cardData.energy)) {
+                    if (!card.used) {
+                        useCard(cardData.atk, player);
+                        if (player === 1) {
+                            player1Energy -= cardData.energy;
+                            updateEnergyDisplay(player1EnergyDisplay, player1Energy);
+                        } else {
+                            player2Energy -= cardData.energy;
+                            updateEnergyDisplay(player2EnergyDisplay, player2Energy);
+                        }
+                        card.classList.add('contrast');
+                        card.used = true;
+                    }
+                }
+            }
+        });
+    }
+
+    return card;
+}
+
+
+function useCard(atk, player) {
+
+    if (player === 1) {
+        if (atk > 0) {
+            player2LifePoints -= atk;
+            player2Life.textContent = player2LifePoints;
+        } else if (atk < 0) {
+            player1LifePoints += atk;
+            player1Life.textContent = player1LifePoints;
+
+            ////////////////////////
+        } else if (atk === 0) {
+            player1LifePoints += 5;
+            player1Life.textContent = player1LifePoints; 
+        }
+        ////////////////////////
+
+        updatePlayer1LifeBar();
+        updatePlayer2LifeBar();
+    } else if (player === 2) {
+        if (atk > 0) {
+            player1LifePoints -= atk;
+            player1Life.textContent = player1LifePoints;
+        } else if (atk < 0) {
+            player2LifePoints += atk;
+            player2Life.textContent = player2LifePoints;
+
+            ///////////////////
+        } else if (atk === 0) {
+            player2LifePoints += 5;
+            player2Life.textContent = player2LifePoints; 
+        }
+
+        updatePlayer2LifeBar();
+        updatePlayer1LifeBar();
+    }
+    checkGameOver();
+}
+
+///////////////// create pop up window for game over
+
+
+function checkGameOver() {
+
+
+    if (player1LifePoints <= 0) {
+        player1LifePoints = 0;
+        player1Life.textContent = player1LifePoints;
+        //player2Life.textContent = player2LifePoints;
+        //updatePlayer2LifeBar();
+        //endTurnButton.disabled = true;
+        updatePlayer1LifeBar();
+        setTimeout(() => {
+            const result = window.confirm('Sorry, you lost!\nDo you want to restart the game?');
+            if (result) {
+                restartGame();
+            }
+        }, 1000);
+    } else if (player2LifePoints <= 0) {
+        player2LifePoints = 0;
+        player2Life.textContent = player2LifePoints;
+        //player1Life.textContent = player1LifePoints;
+        //updatePlayer1LifeBar();
+        //endTurnButton.disabled = true;
+        updatePlayer2LifeBar();
+        setTimeout(() => {
+            const result = window.confirm('Congratulations! You won!\nIsa kang tunay na mamaw!\nDo you want to restart the game?');
+            if (result) {
+                restartGame();
+            }
+        }, 1000);
+    }
+
+}
+
+
+function updatePlayer1LifeBar() {
+    const player1LifeBar = document.querySelector('.player1-life-bar');
+    player1LifeBar.value = player1LifePoints;
+}
+
+function updatePlayer2LifeBar() {
+    const player2LifeBar = document.querySelector('.player2-life-bar');
+    player2LifeBar.value = player2LifePoints;
+}
+
+function updateEnergyDisplay(energyDisplay, energy) {
+    energyDisplay.textContent = energy;
+}
+
+
+function player1Turn() {
+    currentPlayer = 1;
+    assignRandomCardsToPlayer(player1CardsContainer, 1);
+    endTurnButton.disabled = false;
+    player1Energy = 5;
+    updateEnergyDisplay(player1EnergyDisplay, player1Energy);
+    showTurn();
+}
+
+
+function player2Turn() {
+    currentPlayer = 2;
+    endTurnButton.disabled = true;
+    assignRandomCardsToPlayer(player2CardsContainer, 2);
+    player2Energy = 5;
+    updateEnergyDisplay(player2EnergyDisplay, player2Energy);
+    setTimeout(startPlayer2Attack, 2000);
+}
+
+
+function startPlayer2Attack() {
+    if (currentPlayer === 2) {
+        const player2Cards = Array.from(player2CardsContainer.querySelectorAll('.player2-card'));
+        let remainingEnergy = player2Energy;
+
+        while (remainingEnergy > 0 && player2Cards.length > 0) {
+            const randomIndex = Math.floor(Math.random() * player2Cards.length);
+            const card = player2Cards[randomIndex];
+            const cardData = card.textContent.match(/ATK: (-?\d+), Energy: (\d+)/);
+
+            if (cardData) {
+                const atk = parseInt(cardData[1]);
+                const energyCost = parseInt(cardData[2]);
+
+                if (energyCost <= remainingEnergy) {
+                    useCard(atk, 2);
+                    card.classList.add('contrast');
+                    remainingEnergy -= energyCost;
+                }
+
+                player2Cards.splice(randomIndex, 1);
+            }
+        }
+
+        updateEnergyDisplay(player2EnergyDisplay, player2Energy - remainingEnergy);
+        setTimeout(player1Turn, 1000);
+    }
+}
+
+
+endTurnButton.addEventListener('click', () => {
+    if (currentPlayer === 1) {
+        endTurnButton.disabled = true;
+        setTimeout(player2Turn, 500);
+        setTimeout(showTurn, 500);
+    }
+});
+
+
+function restartGame() {
+    player1LifePoints = 100;
+    player2LifePoints = 100;
+    player1Life.textContent = player1LifePoints;
+    player2Life.textContent = player2LifePoints;
+    updatePlayer1LifeBar();
+    updatePlayer2LifeBar();
+
+    player1Energy = 5;
+    player2Energy = 5;
+    updateEnergyDisplay(player1EnergyDisplay, player1Energy);
+    updateEnergyDisplay(player2EnergyDisplay, player2Energy);
+
+    player2CardsContainer.innerHTML = '';
+    for (let i = 0; i < totalCards; i++) {
+        const card = document.createElement('img');
+        card.className = 'player2-card';
+        card.src = 'resources/back.png';
+        player2CardsContainer.appendChild(card);
+    }
+
+    setTimeout(player1Turn, 500);
+}
+
+
+restartGameButton.addEventListener('click', restartGame);
+
+
+function goToHome() {
+    setTimeout(() => {
+        location.reload();
+    }, 200);
+};
+
+homeButton.addEventListener('click', goToHome);
