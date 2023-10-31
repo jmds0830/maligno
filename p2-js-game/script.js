7987259a7ed992502a840d7fae818a9021fa0f88
JM <jmdsegismundo@gmail.com>
2023-10-31 21:01:09 +0800

    feat: updated projrct

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -1,12 +1,14 @@
 const player1CardsContainer = document.querySelector('.player1-cards');
 const player2CardsContainer = document.querySelector('.player2-cards');
-const endTurnButton = document.querySelector('.end-turn-button');
 const player1Life = document.querySelector('.player1-LP');
 const player2Life = document.querySelector('.player2-LP');
 const player1EnergyDisplay = document.querySelector('.player1-energy');
 const player2EnergyDisplay = document.querySelector('.player2-energy');
-const restartGameButton = document.querySelector('.restart-game-button');
 const homeButton = document.querySelector('.home-button');
+const restartGameButton = document.querySelector('.restart-game-button');
+const endTurnButton = document.querySelector('.end-turn-button');
+const homescreen = document.querySelector('.home-screen');
+const playerNameInput = document.querySelector('.input-player-name');
 
 let player1LifePoints = 100;
 let player2LifePoints = 100;
@@ -26,15 +28,56 @@ const cardContent = [
     { name: 'Trap Card 1', atk: 10, energy: 2, image: 'resources/kulam.png' },
     { name: 'Trap Card 2', atk: -5, energy: 2, image: 'resources/sumpa.png' },
     { name: 'Trap Card 3', atk: 0, energy: 2, image: 'resources/agimat.png' },
-
-    ////////add more 2 more kulam and sumpa cards to deck////////
 ];
 
 const back = { image: 'resources/back.png' };
 const trap = { image: 'resources/trap.png' };
 
+const introAudio = document.getElementById('intro-audio');
+const gameAudio = document.getElementById('game-audio');
+const clickAudio = document.getElementById('click-audio');
+const cardSelect = document.getElementById('card-select');
+const hoverAudio = document.getElementById('hover');
+const player1Audio = document.getElementById('player1-audio');
+const player2Audio = document.getElementById('player2-audio');
+const winAudio = document.getElementById('win-audio')
+const winAudio2 = document.getElementById('win-audio2')
+const winAudio3 = document.getElementById('win-audio3')
+const loseAudio = document.getElementById('lose-audio')
+const loseAudio2 = document.getElementById('lose-audio2')
+const buttonHover = document.getElementById('button-hover')
+
+
+const okButton = document.querySelector('.ok-button');
+
+okButton.addEventListener('click', function () {
+    clickAudio.play();
+    removeOpeningMessage();
+});
+
+function removeOpeningMessage() {
+    const openingMessage = document.getElementById('opening-message-container');
+    setTimeout(() => {
+        openingMessage.style.display = 'none';
+    }, 500);  
+    introAudio.play();
+    gameAudio.pause();
+};
+
+okButton.addEventListener('mouseenter', function () {
+    buttonHover.play();
+});
+
+
+playerNameInput.addEventListener('click', function () {
+    clickAudio.play();
+});
+
 
 const startGameButton = document.querySelector('.start-button');
+const howToPlay = document.querySelector('.how-to-play-button');
+const popupWindow = document.querySelector('.popup-window');
+const closeButton = document.querySelector('.close-button');
 
 function startGame() {
 
@@ -45,6 +88,7 @@ function startGame() {
 
     const playerName = playerNameInput.value;
 
+    introAudio.pause();
 
     if (playerName) {
         player1Name.textContent = playerName;
@@ -62,12 +106,19 @@ function startGame() {
         card.src = 'resources/back.png';
         player2CardsContainer.appendChild(card);
     }
-
     showTurn();
     setTimeout(player1Turn, 500);
+
+    clickAudio.play();
 };
 
+gameAudio.volume = 0.7;
+
 startGameButton.addEventListener('click', startGame);
+
+playerNameInput.addEventListener('click', function () {
+    introAudio.play();
+});
 
 
 function showTurn() {
@@ -84,17 +135,19 @@ function showTurn() {
     }
 };
 
-
-const howToPlay = document.querySelector('.how-to-play-button');
-const popupWindow = document.querySelector('.popup-window');
-const closeButton = document.querySelector('.close-button');
-
 howToPlay.addEventListener('click', function () {
-    popupWindow.style.display = 'block';
+    setTimeout(() => {
+        popupWindow.style.display = 'block';
+    }, 300);
+    clickAudio.play();
+
 });
 
 closeButton.addEventListener('click', function () {
-    popupWindow.style.display = 'none';
+    setTimeout(() => {
+        popupWindow.style.display = 'none';
+    }, 300);
+    clickAudio.play();
 });
 
 
@@ -105,7 +158,6 @@ function assignRandomCardsToPlayer(cardsContainer, player) {
         cardsContainer.appendChild(card);
     }
 }
-
 
 let currentPlayer = 1;
 
@@ -121,7 +173,6 @@ function createRandomCard(player) {
     card.textContent = `${cardData.name} (ATK: ${cardData.atk}, Energy: ${cardData.energy})`;
 
     card.used = false;
-
 
     if (cardData.name.startsWith('Trap Card')) {
 
@@ -143,7 +194,7 @@ function createRandomCard(player) {
                             player1Energy -= cardData.energy;
                             updateEnergyDisplay(player1EnergyDisplay, player1Energy);
                         }
-
+                        cardSelect.play();
                         card.classList.add('contrast');
                         card.used = true;
                     }
@@ -167,6 +218,7 @@ function createRandomCard(player) {
                             player2Energy -= cardData.energy;
                             updateEnergyDisplay(player2EnergyDisplay, player2Energy);
                         }
+                        cardSelect.play();
                         card.classList.add('contrast');
                         card.used = true;
                     }
@@ -174,6 +226,15 @@ function createRandomCard(player) {
             }
         });
     }
+
+    card.addEventListener('mouseenter', function () {
+        if (currentPlayer === 1) {
+            hoverAudio.currentTime = 0;
+            hoverAudio.play();
+            hoverAudio.volume = 0.5;
+            gameAudio.play();
+        }
+    });
 
     return card;
 }
@@ -188,14 +249,10 @@ function useCard(atk, player) {
         } else if (atk < 0) {
             player1LifePoints += atk;
             player1Life.textContent = player1LifePoints;
-
-            ////////////////////////
         } else if (atk === 0) {
             player1LifePoints += 5;
-            player1Life.textContent = player1LifePoints; 
+            player1Life.textContent = player1LifePoints;
         }
-        ////////////////////////
-
         updatePlayer1LifeBar();
         updatePlayer2LifeBar();
     } else if (player === 2) {
@@ -205,54 +262,83 @@ function useCard(atk, player) {
         } else if (atk < 0) {
             player2LifePoints += atk;
             player2Life.textContent = player2LifePoints;
-
-            ///////////////////
         } else if (atk === 0) {
             player2LifePoints += 5;
-            player2Life.textContent = player2LifePoints; 
+            player2Life.textContent = player2LifePoints;
         }
-
         updatePlayer2LifeBar();
         updatePlayer1LifeBar();
     }
     checkGameOver();
 }
 
-///////////////// create pop up window for game over
-
-
 function checkGameOver() {
-
 
     if (player1LifePoints <= 0) {
         player1LifePoints = 0;
         player1Life.textContent = player1LifePoints;
-        //player2Life.textContent = player2LifePoints;
-        //updatePlayer2LifeBar();
-        //endTurnButton.disabled = true;
         updatePlayer1LifeBar();
         setTimeout(() => {
-            const result = window.confirm('Sorry, you lost!\nDo you want to restart the game?');
-            if (result) {
-                restartGame();
-            }
+            showModal2();
         }, 1000);
+
     } else if (player2LifePoints <= 0) {
         player2LifePoints = 0;
         player2Life.textContent = player2LifePoints;
-        //player1Life.textContent = player1LifePoints;
-        //updatePlayer1LifeBar();
-        //endTurnButton.disabled = true;
         updatePlayer2LifeBar();
         setTimeout(() => {
-            const result = window.confirm('Congratulations! You won!\nIsa kang tunay na mamaw!\nDo you want to restart the game?');
-            if (result) {
-                restartGame();
-            }
+            showModal1();
         }, 1000);
     }
-
 }
+
+
+function showModal1() {
+    const modal1 = document.getElementById('modal1');
+    modal1.classList.remove('hide1');
+    winAudio.play();
+    winAudio2.play();
+    winAudio3.play();
+}
+function hideModal1() {
+    const modal1 = document.getElementById('modal1');
+    modal1.classList.add('hide1');
+}
+
+function showModal2() {
+    const modal2 = document.getElementById('modal2');
+    modal2.classList.remove('hide2');
+    loseAudio.play();
+    loseAudio2.play();
+}
+function hideModal2() {
+    const modal2 = document.getElementById('modal2');
+    modal2.classList.add('hide2');
+}
+
+
+const restartGameButton1 = document.querySelector('#modal1 .modal-restart-button');
+const restartGameButton2 = document.querySelector('#modal2 .modal-restart-button');
+
+restartGameButton1.addEventListener('click', function () {
+    clickAudio.play()
+    const modal1 = document.getElementById('modal1');
+    setTimeout(() => {
+        modal1.classList.add('hide1');
+        restartGame();
+    }, 500);
+    gameAudio.currentTime = 0;
+});
+
+restartGameButton2.addEventListener('click', function () {
+    clickAudio.play()
+    const modal2 = document.getElementById('modal2');
+    setTimeout(() => {
+        modal2.classList.add('hide2');
+        restartGame();
+    }, 500);
+    gameAudio.currentTime = 0;
+});
 
 
 function updatePlayer1LifeBar() {
@@ -272,6 +358,7 @@ function updateEnergyDisplay(energyDisplay, energy) {
 
 function player1Turn() {
     currentPlayer = 1;
+    player1Audio.play();
     assignRandomCardsToPlayer(player1CardsContainer, 1);
     endTurnButton.disabled = false;
     player1Energy = 5;
@@ -279,10 +366,10 @@ function player1Turn() {
     showTurn();
 }
 
-
 function player2Turn() {
     currentPlayer = 2;
     endTurnButton.disabled = true;
+    player2Audio.play();
     assignRandomCardsToPlayer(player2CardsContainer, 2);
     player2Energy = 5;
     updateEnergyDisplay(player2EnergyDisplay, player2Energy);
@@ -313,7 +400,7 @@ function startPlayer2Attack() {
                 player2Cards.splice(randomIndex, 1);
             }
         }
-
+        cardSelect.play();
         updateEnergyDisplay(player2EnergyDisplay, player2Energy - remainingEnergy);
         setTimeout(player1Turn, 1000);
     }
@@ -321,13 +408,12 @@ function startPlayer2Attack() {
 
 
 endTurnButton.addEventListener('click', () => {
+    clickAudio.play()
     if (currentPlayer === 1) {
-        endTurnButton.disabled = true;
         setTimeout(player2Turn, 500);
         setTimeout(showTurn, 500);
     }
 });
-
 
 function restartGame() {
     player1LifePoints = 100;
@@ -353,14 +439,56 @@ function restartGame() {
     setTimeout(player1Turn, 500);
 }
 
+restartGameButton.addEventListener('click', function () {
+    clickAudio.play()
+    restartGame();
+});
 
-restartGameButton.addEventListener('click', restartGame);
-
+homeButton.addEventListener('click', goToHome);
 
 function goToHome() {
+    clickAudio.play()
     setTimeout(() => {
         location.reload();
     }, 200);
 };
 
-homeButton.addEventListener('click', goToHome);
+startGameButton.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+howToPlay.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+closeButton.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+homeButton.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+restartGameButton.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+endTurnButton.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+restartGameButton1.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
+
+restartGameButton2.addEventListener("mouseenter", function () {
+    buttonHover.currentTime = 0;
+    buttonHover.play();
+});
